aac782686e412fce6dce166911d4f90385e52523
Tiffany Huang <tiffany.jenyun.huang@gmail.com>
2020-01-12 17:17:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -10,9 +10,9 @@ def display_board
   puts "   |   |   "
 end
 
-def #position_taken?(board, input)
-  if move == "X" || move == "O"
-    puts "X" || "O"
+def position_taken?(board, input)
+  if board[input] == "X" || board[input] == "O"
+    true
   else
       puts "invalid"
   end
